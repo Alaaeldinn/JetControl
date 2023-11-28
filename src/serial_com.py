b8d13c7f6449a84913bf1f3049079f9cbc2ab05a
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-11-28 20:25:20 +0000

    Update serial_com.py

--- src/serial_com.py
+++ src/serial_com.py
@@ -1,7 +1,6 @@
-# serial_com.py
-
 import serial
 import logging
+import queue
 
 class SerialCom:
     """Handles serial communication with an Arduino."""
@@ -9,14 +8,21 @@ class SerialCom:
     def __init__(self, port, baud_rate):
         try:
             self.ser = serial.Serial(port, baud_rate)
+            self.message_queue = queue.Queue()
             logging.info(f"Serial connection established on port {port}")
         except serial.SerialException as e:
             logging.error(f"Error: Unable to open serial port - {e}")
             # Handle the exception or exit gracefully
 
     def send_data(self, data):
-        """Send data over the serial connection."""
-        self.ser.write(data.encode())
+        """Queue data to be sent over the serial connection."""
+        self.message_queue.put(data)
+
+    def process_queue(self):
+        """Send queued data over the serial connection."""
+        while not self.message_queue.empty():
+            message = self.message_queue.get()
+            self.ser.write(message.encode())
 
     def close_serial_connection(self):
         """Close the serial connection."""
